f69bdcef7c0eac1d6f8b04b91568c01b8b7db11b
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-09-02 01:35:44 +0000

    solve find min.py

--- findmin.py
+++ findmin.py
@@ -21,3 +21,21 @@ def find_min(nums):
           if i< min:
               min = i
     return min
+
+# don't touch below this line
+
+
+def test(nums):
+    res = find_min(nums)
+    print(f"Follower counts: {nums}")
+    print(f"Lowest follower count: {res}")
+    print("----")
+
+
+def main():
+    test([7, 4, 3, 100, 2343243, 343434, 1, 2, 32])
+    test([12, 12, 12])
+    test([10, 200, 3000, 5000, 4])
+
+
+main()
